a274ed2ff3980e6979208e61af6dd2810f145c81
Téa Wright <teawright@gmail.com>
2026-05-26 22:43:43 +0900

    fixing syntax err

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // If your site is served at https://<username>.github.io/<repo>/
-  // uncomment the next line and set the repo name correctly:
-  // base: '/learning-situated-interaction.github.io/',
+  base: './',
 })
